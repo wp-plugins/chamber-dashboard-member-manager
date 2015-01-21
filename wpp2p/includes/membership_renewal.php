a14ff6916d0466a868c98e30df9d4d3da5c65ae0
Gwendydd <Gwendydd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-21 00:13:04 +0000

    removing duplicate files
    git-svn-id: https://plugins.svn.wordpress.org/chamber-dashboard-member-manager/trunk@1072081 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpp2p/includes/membership_renewal.php
+++
@@ -1,5 +0,0 @@
-<div class="my_meta_control">
-		<?php $mb->the_field('renewal_date'); ?>
-		<label><?php _e('Next Membership Renewal Date', 'cdashmm'); ?></label>
-		<p><input type="text" name="<?php $mb->the_name(); ?>" value="<?php $mb->the_value(); ?>" id="renewal_date" /></p> 
-</div>
