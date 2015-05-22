d5c604c08f2db8e4bafef0078892a60f0c5317c5
Gwendydd <Gwendydd@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-22 03:58:16 +0000

    tagging 1.8.5
    git-svn-id: https://plugins.svn.wordpress.org/chamber-dashboard-member-manager/tags/1.8.5@1165453 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/aside.php
+++ includes/aside.php
@@ -11,10 +11,10 @@
 		<li><a href="https://chamberdashboard.com/professional-services-support/?utm_source=plugin&utm_medium=sidebar&utm_campaign=member-manager#maintenance" target="_blank"><?php _e('Worry-Free Maintenance:', 'cdashmm'); ?></a><?php _e(' let us take care of maintaining your site, so you can focus on what you do best!', 'cdashmm'); ?></li>
 	</ul>
 	<h3><?php _e('More Amazing Plugins', 'cdashmm'); ?></h3>
-	<!-- <p><?php _e('Increase visibility for your members with the <a href=https://chamberdashboard.com/features/business-directory/" target="_blank">Chamber Dashboard Business Directory.</a>', 'cdashmm'); ?></p> -->
+	<p><span style="font-weight: bold; color: #16A022"><?php _e( 'New! ', 'cdashrp' ); ?></span><?php _e('Automate membership renewal <a href=https://chamberdashboard.com/downloads/recurring-payments/?utm_source=plugin&utm_medium=sidebar&utm_campaign=member-manager" target="_blank">Recurring Payments</a> plugin!  Members can sign up for recurring payments. Automatically send renewal notices and overdue invoice reminders.', 'cdashmm'); ?></p>
 	<p><?php _e('Engage your community in local events, with our <a href="https://chamberdashboard.com/features/chamber-dashboard-events-calendar/?utm_source=plugin&utm_medium=sidebar&utm_campaign=member-manager" target="_blank">event calendar</a> plugin.', 'cdashmm'); ?></p>
-	<p><?php _e('Accept <a href="https://chamberdashboard.com/features/chamber-dashboard-member-manager/?utm_source=plugin&utm_medium=sidebar&utm_campaign=member-manager" target="_blank">Member Payments</a> and invoice your members, straight from your site.  Save staff time with auto-invoicing.', 'cdashmm'); ?></p>
 	<p><?php _e('<a href="https://chamberdashboard.com/features/crm/?utm_source=plugin&utm_medium=sidebar&utm_campaign=member-manager" target="_blank">Track and Catalog your Contacts</a> – Trade your collection of Excel spreadsheets for a CRM that lets you track calls and meeting attendance, categorize your contacts, gives your staff shared access to the same data and more.', 'cdashmm'); ?></p>  
+
 
 	<h3><?php _e('Connect', 'cdashmm'); ?></h3>
 	<p><?php _e('<a href="https://chamberdashboard.com/chamber-dashboard-user-map/?utm_source=plugin&utm_medium=sidebar&utm_campaign=member-manager" target="_blank">Get On the Map</a> – We have users around the world!  We\'d love to link to your site too.', 'cdashmm'); ?></p>
